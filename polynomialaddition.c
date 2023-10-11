27f8e3c272c8b0ee26c91ddd0bc8a6d5a091a6a7
vcgithub41210 <113379660+vcgithub41210@users.noreply.github.com>
2023-10-11 20:22:16 +0000

    program to add two polynomials

---
+++ polynomialaddition.c
@@ -0,0 +1,83 @@
+// to read two polynomials and find the sum of the polynomials
+
+#include <stdio.h>
+
+//array of structures
+
+struct Term{
+    int coefficient;
+    int varpower;
+};
+
+void Insertion_Sort_Structure(struct Term *polynomial,int size){
+    for(int i = 1; i < size;i++)
+    {
+        struct Term temp = polynomial[i];
+        int j = i-1;
+        while(j>=0 && polynomial[j].varpower > temp.varpower)
+        {
+            polynomial[j+1] = polynomial[j];
+            j--;
+        }
+        polynomial[j+1] = temp;
+    }
+}
+
+int main(){
+
+    //input the sizes and polynomials
+    int size1,size2;
+    printf("Enter the no of terms of polynomial 1: ");
+    scanf("%d",&size1);
+    struct Term poly1[size1];
+    printf("Enter the terms of the polynomial in the form:=> coefficient,degree:\n");
+    for(int i = 0; i < size1;i++)
+    {
+        scanf("%d,%d",&poly1[i].coefficient,&poly1[i].varpower);
+    }
+    Insertion_Sort_Structure(poly1,size1);
+    printf("Enter the no of terms of polynomial 2: ");
+    scanf("%d",&size2);
+    struct Term poly2[size2];
+    printf("Enter the terms of the polynomial in the form:=> coefficient,degree:\n");
+    for(int i = 0; i < size2;i++)
+    {
+        scanf("%d,%d",&poly2[i].coefficient,&poly2[i].varpower);
+    }
+    Insertion_Sort_Structure(poly2,size2);
+
+    //initialising result array
+    struct Term result[size1+size2];
+    //adding polynomial 1 to result
+    for(int i = 0; i < size1;i++) result[i] = poly1[i];
+    int length = size1;
+    //adding polynomial 2 to result
+    for(int i = 0; i < size2; i++)
+    {
+        int power = poly2[i].varpower;
+        int start = 0;
+        int end = size1-1;
+        int middle;
+        while(start <= end)
+        {
+            middle = (start+end)/2;
+            if(result[middle].varpower == power) break;
+            else if(result[middle].varpower > power) end = middle-1;
+            else start = middle+1;
+        }
+        if(result[middle].varpower == power) result[middle].coefficient += poly2[i].coefficient;
+        else{
+            result[length].coefficient = poly2[i].coefficient;
+            result[length].varpower = poly2[i].varpower;
+            length++;
+        }
+    }
+    Insertion_Sort_Structure(result,length);
+    for(int i = length-1; i > -1;i--)
+    {
+        result[i].varpower!=0?printf("%dx^%d",result[i].coefficient,result[i].varpower):printf("%d",result[i].coefficient);
+        if(i!=0) printf(" + ");
+    }
+    //get the maximum degree of both polynomials so that we can print only those values
+    //that are upto the highest degree
+}
