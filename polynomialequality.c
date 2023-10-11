20879bc56596ab4d6e323be11fd0338fc2336776
vcgithub41210 <113379660+vcgithub41210@users.noreply.github.com>
2023-10-11 20:37:45 +0000

    program to evaluate a polynomial

---
+++ polynomialequality.c
@@ -0,0 +1,52 @@
+// to read two polynomials and find the sum of the polynomials
+
+#include <stdio.h>
+
+//array of structures
+
+struct Term{
+    int coefficient;
+    int varpower;
+};
+
+void Insertion_Sort_Structure(struct Term *polynomial,int size){
+    for(int i = 1; i < size;i++)
+    {
+        struct Term temp = polynomial[i];
+        int j = i-1;
+        while(j>=0 && polynomial[j].varpower > temp.varpower)
+        {
+            polynomial[j+1] = polynomial[j];
+            j--;
+        }
+        polynomial[j+1] = temp;
+    }
+}
+
+int main(){
+
+    //input the sizes and polynomials
+    int size,sum = 0,x;
+    printf("Enter the no of terms of polynomial 1: ");
+    scanf("%d",&size);
+    struct Term poly[size];
+    printf("Enter the terms of the polynomial in the form:=> coefficient,degree:\n");
+    for(int i = 0; i < size;i++)
+    {
+        scanf("%d,%d",&poly[i].coefficient,&poly[i].varpower);
+    }
+    Insertion_Sort_Structure(poly,size);
+    printf("Enter the value of x: ");
+    scanf("%d",&x);
+    for(int i = 0; i < size; i++)
+    {
+        int power = poly[i].varpower;
+        int product = poly[i].coefficient;
+        for(int j = 0; j < power;j++)
+        {
+            product *= x;
+        }
+        sum += product;
+    }
+    printf("***Value of polynomial for x = %d***\n\t\t%d\n",x,sum);
+}
