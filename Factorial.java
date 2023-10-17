198d9903127e92f9569e138487d57ec68e2d0295
vcgithub41210 <113379660+vcgithub41210@users.noreply.github.com>
2023-10-17 09:50:50 +0000

    Recursin using java

---
+++ Factorial.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.Scanner;
+
+class Factorial
+{
+	public static int recursion(int number){
+		if(number == 1) return 1;
+		if(number < 1) return 0;
+		return number * recursion(number-1);	
+	}
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number: ");
+		int number = sc.nextInt();
+		int factorial = recursion(number);
+		System.out.println("***Factorial of " + number + " ***");
+		System.out.println(factorial);
+	}
+}
