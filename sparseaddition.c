bb368e95846ac1f8e7d5e521d2db1492715b20df
vcgithub41210 <113379660+vcgithub41210@users.noreply.github.com>
2023-10-19 06:17:27 +0000

    created functions for sparseaddition and sparse representation conversion

--- sparseaddition.c
+++ sparseaddition.c
@@ -1,3 +1,9 @@
+/*
+ this program is used for adding two sparse matrices and displaying the result
+ Time complexity ⏰️ :- i dont know honestly
+ Space complexity 🎮️ :- i dont know honestly
+*/
+
 #include <stdio.h>
 
 //Display function
@@ -12,152 +18,137 @@ int Display(int array[][3],int row,int column){
     }
 }
 
+int sparseRepresentation(int array[][10],int sparse[][3],int row,int column){
+	int count = 0;
+	sparse[0][0] = row;
+	sparse[0][1] = column;
+	for(int i = 0; i < row; i++)
+	{
+		for(int j = 0; j < column; j++)
+		{
+			if(array[i][j] != 0)
+			{
+				count++;
+				sparse[count][0] = i;
+				sparse[count][1] = j;
+				sparse[count][2] = array[i][j];
+			}
+		}
+	}
+	sparse[0][2] = count;
+	return count;
+}
+
+int sparseAddition(int finalsparse[][3],int sparse1[][3],int sparse2[][3],int row1 ,int column1,int count1,int count2){
+	finalsparse[0][0] = row1;
+	finalsparse[0][1] = column1;
+	int current = 1;
+	int x = 1;
+	int y = 1;
+	while(x< count1 || y< count2)
+	{
+		if(x == count1 || y == count2)
+		{
+			if(x==count1){
+				finalsparse[current][0] = sparse2[y][0];
+				finalsparse[current][1] = sparse2[y][1];
+				finalsparse[current][2] = sparse2[y][2];
+				y++;
+			}
+			else{
+				finalsparse[current][0] = sparse1[x][0];
+				finalsparse[current][1] = sparse1[x][1];
+				finalsparse[current][2] = sparse1[x][2];
+				x++;
+			};
+		}else if(sparse1[x][0] == sparse2[y][0])
+		{
+			if(sparse1[x][1] == sparse2[y][1])
+			{
+				finalsparse[current][0] = sparse2[y][0];
+				finalsparse[current][1] = sparse2[y][1];
+				finalsparse[current][2] = sparse2[y][2] + sparse1[x][2];
+				y++;
+				x++;
+			}
+			else if(sparse1[x][1] > sparse2[y][1]){
+				finalsparse[current][0] = sparse2[y][0];
+				finalsparse[current][1] = sparse2[y][1];
+				finalsparse[current][2] = sparse2[y][2];
+				y++;
+			}else
+			{
+				finalsparse[current][0] = sparse1[x][0];
+				finalsparse[current][1] = sparse1[x][1];
+				finalsparse[current][2] = sparse1[x][2];
+				x++;
+			}
+		}else if(sparse1[x][0] > sparse2[y][0])
+		{
+			finalsparse[current][0] = sparse2[y][0];
+			finalsparse[current][1] = sparse2[y][1];
+			finalsparse[current][2] = sparse2[y][2];
+			y++;
+		}else
+		{
+			finalsparse[current][0] = sparse1[x][0];
+			finalsparse[current][1] = sparse1[x][1];
+			finalsparse[current][2] = sparse1[x][2];
+			x++;
+		}
+		current++;
+	}
+	finalsparse[0][2] = current-1;
+	return current;
+	
+}
 
 int main()
 {
-    //FIRST MATRIX+++++++++++
-    
-    
-    
+    //FIRST MATRIX
     int row1,column1;
     printf("Enter the number of rows: ");
     scanf("%d",&row1);
     printf("Enter the number of columns: ");
     scanf("%d",&column1);
-    int mat1[10][10];
-
+    int matrix1[10][10];
     //inputing first matrix
     printf("Enter the elements of the first matrix:\n");
     for(int i = 0;i<row1;i++)
     {
         for(int j=0; j < column1;j++)
         {
-            scanf("%d",&mat1[i][j]);
+            scanf("%d",&matrix1[i][j]);
         }
     }
-
     //converting to sparse representation
-    int sparse1[10][3];
-    int count1 = 1;
-    sparse1[0][0] = row1;
-    sparse1[0][1] = column1;
-
-    for(int i = 0; i < row1; i++)
-    {
-        for(int j = 0; j < column1; j++)
-        {
-            if(mat1[i][j] != 0)
-            {
-                sparse1[count1][0] = i;
-                sparse1[count1][1] = j;
-                sparse1[count1][2] = mat1[i][j];
-                count1++;
-            }
-        }
-        printf("\n");
-    }
-    sparse1[0][2] = count1-1;
-
-    //SECOND MATRIX******************
+    int sparseForm1[10][3];
+    int count1 = sparseRepresentation(matrix1,sparseForm1,row1,column1);
 
 
-
-
-
+    //SECOND MATRIX
     int row2 = row1,column2 = column1;
-    int mat2[10][10];
-
-    //inputing first matrix
-    printf("Enter the elements of the first matrix:\n");
+    int matrix2[10][10];
+    //inputing second matrix
+    printf("Enter the elements of the second matrix:\n");
     for(int i = 0;i<row2;i++)
     {
         for(int j=0; j < column2;j++)
         {
-            scanf("%d",&mat2[i][j]);
+            scanf("%d",&matrix2[i][j]);
         }
     }
-    int sparse2[10][3];
     //converting to sparse representation
-    int count2 = 1;
-    sparse2[0][0] = row2;
-    sparse2[0][1] = column2;
-    for(int i = 0; i < row2; i++)
-    {
-        for(int j = 0; j < column2; j++)
-        {
-            if(mat2[i][j] != 0)
-            {
-                sparse2[count2][0] = i;
-                sparse2[count2][1] = j;
-                sparse2[count2][2] = mat2[i][j];
-                count2++;
-            }
-        }
-        printf("\n");
-    }
-    sparse2[0][2] = count2-1;
-    //Addition of matrices
-    int finalsparse[count1+count2][3];
-    finalsparse[0][0] = row1;
-    finalsparse[0][1] = column1;
-    int current = 1;
-    int x = 1,y=1;
-    while(x < count1 || y < count2)
-    {
-        if(x == count1 || y == count2)
-        {
-            if(x == count1){
-                finalsparse[current][0] = sparse2[y][0];
-                finalsparse[current][1] = sparse2[y][1];
-                finalsparse[current][2] = sparse2[y][2];
-                y++;
-            }
-            else{
-                finalsparse[current][0] = sparse1[x][0];
-                finalsparse[current][1] = sparse1[x][1];
-                finalsparse[current][2] = sparse1[x][2];
-                x++;
-            };
-        }
-        else if (sparse1[x][0] == sparse2[y][0])
-        {
-            if(sparse1[x][1] == sparse2[y][1])
-            {
-                finalsparse[current][0] = sparse1[x][0];
-                finalsparse[current][1] = sparse1[x][1];
-                finalsparse[current][2] = sparse1[x][2] + sparse2[y][2];
-                x++;
-                y++;
-            }
-            else if(sparse1[x][1] > sparse2[y][1]){
-                finalsparse[current][0] = sparse2[y][0];
-                finalsparse[current][1] = sparse2[y][1];
-                finalsparse[current][2] = sparse2[y][2];
-                y++;
-            }else{
-                finalsparse[current][0] = sparse1[x][0];
-                finalsparse[current][1] = sparse1[x][1];
-                finalsparse[current][2] = sparse1[x][2];
-                x++;
-            }
-        }
-        else if(sparse1[x][0] > sparse2[y][0])
-        {
-            finalsparse[current][0] = sparse2[y][0];
-            finalsparse[current][1] = sparse2[y][1];
-            finalsparse[current][2] = sparse2[y][2];
-            y++;
-        }
-        else{
-            finalsparse[current][0] = sparse1[x][0];
-            finalsparse[current][1] = sparse1[x][1];
-            finalsparse[current][2] = sparse1[x][2];
-            x++;
-        }
-        current++;
-    }
-    finalsparse[0][2] = current-1;
+    int sparseForm2[10][3];
+    int count2 = sparseRepresentation(matrix2,sparseForm2,row2,column2);
     
-    printf("The final sparse matrix is:\n");
-    Display(finalsparse,current,3);
+    
+    //ADDITION
+    int finalsparseForm[20][3];
+    int current = sparseAddition(finalsparseForm,sparseForm1,sparseForm2,row1,column1,count1+1,count2+1);
+
+
+    //DISPLAY OUTPUT   
+    printf("***FINAL SPARSE MATRIX****\n");
+    Display(finalsparseForm,current,3);
 }
