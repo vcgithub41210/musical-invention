1cb1cc13c01c1efe19abe1c440b52594b99d5097
vcgithub41210 <113379660+vcgithub41210@users.noreply.github.com>
2023-11-22 20:58:40 +0530

    Update LinkedList.c

--- LinkedList.c
+++ LinkedList.c
@@ -32,12 +32,12 @@ void AddAfter(){
     Node *new_node = (Node *)malloc(sizeof(Node));
     printf("Enter the data of the reference node: ");
     scanf("%d",&value);
-    printf("Enter the data of the new node: ");
-    scanf("%d",&new_node->data);
     Node *ptr = head;
     while(ptr != NULL)
     {
         if(ptr->data == value){
+            printf("Enter the data of the new node: ");
+            scanf("%d",&new_node->data);
             new_node->next = ptr->next;
             ptr->next = new_node;
             return;
@@ -51,17 +51,19 @@ void AddBefore(){
     Node *new_node = (Node *)malloc(sizeof(Node));
     printf("Enter the data of the reference node: ");
     scanf("%d",&value);
-    printf("Enter the data of the new node: ");
-    scanf("%d",&new_node->data);
-    Node *ptr = head;
     if(head->data == value){
+        printf("Enter the data of the new node: ");
+        scanf("%d",&new_node->data);
         new_node->next = head;
         head = new_node;
         return;
     }
+    Node *ptr = head;
     while(ptr->next != NULL)
     {
         if(ptr->next->data == value){
+            printf("Enter the data of the new node: ");
+            scanf("%d",&new_node->data);
             new_node->next = ptr->next;
             ptr->next = new_node;
             return;
@@ -75,17 +77,19 @@ void AddAtPos(){
     Node *new_node = (Node*)malloc(sizeof(Node));
     printf("Enter the position at which node is to be added: ");
     scanf("%d",&position);
-    printf("Enter the data of the new node: ");
-    scanf("%d",&new_node->data);
-    Node *ptr = head;
-    int count = 1;
     if(position == 1){
+        printf("Enter the data of the new node: ");
+        scanf("%d",&new_node->data);
         new_node->next = head;
         head = new_node;
         return;
     }
+    Node *ptr = head;
+    int count = 1;
     while(ptr != NULL){
-        if(count == position-1){
+        if(count == position-1 && ptr->next != NULL){
+            printf("Enter the data of the new node: ");
+            scanf("%d",&new_node->data);
             new_node->next = ptr->next;
             ptr->next = new_node;
             return;
@@ -96,46 +100,129 @@ void AddAtPos(){
     printf("Positon out of range\n");
 }
 
+void DeleteNode(){
+    printf("Enter the data of the node to be deleted: ");
+    scanf("%d",&value);
+    if(head->data == value) head = head->next;
+    else{
+        Node *ptr = head;
+        while(ptr->next != NULL){
+            if(ptr->next->data == value){
+                ptr->next = ptr->next->next;
+                return;
+            }
+            ptr = ptr->next;
+        }
+        printf("Node does not exist\n");
+    }
+}
 
+void DeleteAtPos(){
+    printf("Enter the position of the node to be deleted (head has position 1)");
+    scanf("%d",&position);
+    if(position == 1) head = head->next;
+    else{
+        Node *ptr = head;
+        int count = 1;
+        while(ptr->next != NULL){
+            if(count == position-1){
+                ptr->next = ptr->next->next;
+                return;
+            }
+            count++;
+            ptr = ptr->next;
+        }
+        printf("Position out of range\n");
+    }
+}
+
+void DeleteSmallestNode(){
+    int min = head->data;
+    Node *minptr = head;
+    Node *ptr = head;
+    if(head->next == NULL){
+        head = head->next;
+        return;
+    }
+    while(ptr->next != NULL){
+        if(ptr->next->data < min){
+            min = ptr->next->data;
+            minptr = ptr;
+        }
+        ptr = ptr->next;
+    }
+    if(min == head->data)head = head->next;
+    else{
+        minptr->next = minptr->next->next;
+    }
+}
+
+void SearchNode(){
+    printf("Enter the data of the node to be searched: ");
+    scanf("%d",&value);
+    Node *ptr = head;
+    position = 1;
+    while(ptr != NULL){
+        if(ptr->data == value){
+            printf("The node is present at: %d\n",position);
+            return;
+        }
+        ptr = ptr->next;
+        position++;
+    }
+    printf("Node not found\n");
+}
 
 void Display(){
+    if(head == NULL){
+        printf("List is empty\n");
+        return;
+    }
     Node *ptr = head;
-    while(ptr != NULL)
-    {
+    while(ptr != NULL){
         printf("[%d]->",ptr->data);
         ptr = ptr->next;
     }
     printf("\n");
-    
 }
+
 int main(){
-    AddEnd();
-    Display();
-    AddAfter();
-    Display();
-    AddBefore();
-    Display();
-    AddAtPos();
-    Display();
-//     int operand;
-//     printf("Choose the following:\n");
-//     printf("(1)AddToEnd\n(2)AddAfterNode\n(3)AddBeforeNode\n(4)AddAtPosition\n(5)DeleteNode\n(6)DeleteAtPosition\n(7)DeleteSmallest\n(8)SearchData\n");
-//     do{
-//         printf("=> ");
-//         scanf("%d",operand);
-//         if (operand == 1){
-//             AddEnd();
-//         }
-//         else if(head == NULL){
-//             printf("list is empty!");
-//         }
-//         else{
-//             switch(operand){
-//                 case 2:
-//                     AddAfter();
-//                     break;
-//             }
-//         }
-//         Display();
-//     }while(operand != 0);
+    int operand;
+    printf("(1)AddEnd\t(2)AddAfter\t(3)AddBefore\n(4)AddAtPosition\t(5)DeleteNode\t(6)DeleteAtPosition\n(7)DeleteSmallestNode\t(8)SearchForNode\t(9)Quit\n");
+    while(operand != 9){
+        printf("=> ");
+        scanf("%d",&operand);
+        if(operand == 1) AddEnd();
+        else if(head == NULL) printf("list is empty\n");
+        else{
+            switch(operand){
+                case 2:
+                    AddAfter();
+                    break;
+                case 3:
+                    AddBefore();
+                    break;
+                case 4:
+                    AddAtPos();
+                    break;
+                case 5:
+                    DeleteNode();
+                    break;
+                case 6:
+                    DeleteAtPos();
+                    break;
+                case 7:
+                    DeleteSmallestNode();
+                    break;
+                case 8:
+                    SearchNode();
+                    break;
+                case 9:
+                    continue;
+                default:
+                    printf("Invalid operand\n");
+            }
+        }
+        Display();
+    }
 }
