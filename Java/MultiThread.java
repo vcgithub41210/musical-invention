628d3739f13aa08f71e0d0e31597efbc9ff49e94
vcgithub41210 <113379660+vcgithub41210@users.noreply.github.com>
2023-12-03 20:05:32 +0000

    learning threads in java

---
+++ Java/MultiThread.java
@@ -0,0 +1,56 @@
+import java.io.*;
+ 
+public class MultiThread {
+    static int num = 1;
+
+   public static class Thread1 extends Thread {
+       public void run() {
+           while(true) {
+               try {
+                  Thread.sleep(1000);
+                  num += 1;
+               } catch (InterruptedException e) {
+                  e.printStackTrace();
+               }
+           }
+       }
+   }
+ 
+   public static class Thread2 extends Thread {
+       public void run() {
+           while(true) {
+               if((num)%2 == 0)
+                  System.out.println(num * num);
+                try {
+                  Thread.sleep(1000);
+               } catch (InterruptedException e) {
+                  e.printStackTrace();
+               }
+           }
+       }
+   }
+ 
+   public static class Thread3 extends Thread {
+       public void run() {
+           while(true) {
+               if((num)%2 != 0)
+                  System.out.println(num*num*num);
+                try {
+                  Thread.sleep(1000);
+               } catch (InterruptedException e) {
+                  e.printStackTrace();
+               }
+           }
+       }
+   }
+ 
+   public static void main(String args[]) {
+       Thread1 t1 = new Thread1();
+       Thread2 t2 = new Thread2();
+       Thread3 t3 = new Thread3();
+       t1.start();
+       t2.start();
+       t3.start();
+   }
+}
+ 
