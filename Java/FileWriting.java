24789b46465156a2f07bd765aa05b569dbaa9d1d
vcgithub41210 <113379660+vcgithub41210@users.noreply.github.com>
2023-12-03 16:09:51 +0000

    Writing into file using FileOutputStream

--- Java/FileWriting.java
+++ Java/FileWriting.java
@@ -11,33 +11,32 @@ class FileWriting{
             else{
                 System.out.println("File already exists!");
             }
-        }
-        catch(IOException e){
-            System.out.println("Some error has occured!");
-            e.printStackTrace();
-        }
-        try{
             Scanner sc = new Scanner(System.in);
-            FileWriter newWrite = new FileWriter(myObj);
+            FileOutputStream fout = new FileOutputStream(myObj);
+
             System.out.print("Enter the name of the person: ");
-            newWrite.write("Name: "+ sc.nextLine()+"\n");
+            fout.write(("Name: " + sc.nextLine() +"\n").getBytes());
+
             System.out.print("Enter the age of the person: ");
-            newWrite.write("Age: "+ sc.nextLine()+"\n");
+            fout.write(("Age: " + sc.nextLine() + "\n").getBytes());
+
             System.out.print("Enter the profession of the person: ");
-            newWrite.write("Profession: "+ sc.nextLine()+"\n");
-            newWrite.close();
-        }
-        catch(IOException e){
-            System.out.println("Failed to write into the file!");
-            e.printStackTrace();
-        }
-        try{
-            Scanner newScanner = new Scanner(myObj);
-            while(newScanner.hasNextLine()){
-                String data = newScanner.nextLine();
-                System.out.println(data);
+            fout.write(("Profession: "+sc.nextLine() + "\n").getBytes());
+
+            fout.close();
+
+            FileInputStream fin = new FileInputStream(myObj);
+            int a = fin.read();
+            while(a != -1){
+                System.out.print((char) a);
+                a = fin.read();
             }
-            newScanner.close();
+            // Scanner newScanner = new Scanner(myObj);
+            // while(newScanner.hasNextLine()){
+            //     String data = newScanner.nextLine();
+            //     System.out.println(data);
+            // }
+            // newScanner.close();
         }
         catch(IOException e){
             System.out.println("Some error has occured!");
