fa8519ff475d3477e51f446f3f341566d098f688
vcgithub41210 <113379660+vcgithub41210@users.noreply.github.com>
2024-05-17 12:58:01 +0530

    Update Btree.c

--- fun projects/Btree.c
+++ fun projects/Btree.c
@@ -11,7 +11,7 @@
 typedef struct node{
   int length; // to keep track of length of the array
   int array[5]; //array of data
-  struct node *pointer[5]; //array of pointers
+  struct node *pointer[6]; //array of pointers
 } node;
 
 node *root; //root node of the tree 
@@ -32,22 +32,72 @@ int binary_search(int data,int array[],int length){
 }
 
 //[1,3,7,12,14]
-int insert_in_node(node *root){
-  int data;
-  int length = root->length;
-  printf("Enter the data to be inserted: ");
-  scanf("%d",&data);
-  if (length == 0){
-    root->array[0] = data;
-    root->length++;
+int insert_into_node(node *nd,int data,int index,node *ptr){
+  if(nd == root && nd->length == 0){
+    nd->array[0] = data;
   }
   else{
-    int index = binary_search(data,root->array,length); //to find the correct pointer where 
-    for(int i = length; i > index;i--){
-      root->array[i] = root->array[i-1];
+    nd->pointer[nd->length + 1] = nd->pointer[nd->length];
+    for(int i = nd->length; i > index; i--){
+      nd->array[i] = nd->array[i-1];
+      if(nd->pointer[i])nd->pointer[i+1] = nd->pointer[i];
     }
-    root->array[index] = data;
-    root->length++;
+    nd->array[index] = data;
+    nd->pointer[index] = ptr;
+  }
+  nd->length++;
+  printf("Hello\tHello\n");
+}
+void transfer_data(node *temp,node *nd,int start,int end){
+  int j = 0;
+  for(int i = start;i < end;i++){
+    temp->array[j] = nd->array[i];
+    if(nd->pointer[j])temp->pointer[j] = nd->pointer[j];
+    j++;
+  }
+  temp->pointer[j] = nd->pointer[j];
+  temp->length = 2;
+}
+node *split_node(node *nd){
+  node *tmp1 = (node*)malloc(sizeof(node));
+  transfer_data(tmp1,nd,0,2);
+  node *tmp2 = (node*)malloc(sizeof(node));
+  transfer_data(tmp2,nd,3,5); 
+  node *tmp = (node*)malloc(sizeof(node));
+  tmp->array[0] = nd->array[2];
+  tmp->pointer[0] = tmp1;
+  tmp->pointer[1] = tmp2;
+  return tmp;
+}
+node *traverse_node(node *root,int data){
+  int length = root->length;
+  if (length == 0){
+    insert_into_node(root,data,0,NULL);
+  }
+  else{
+    node *temp;
+    int index = binary_search(data,root->array,length); //to find the correct pointer where 
+    if(root->pointer[index] != NULL){
+      temp = traverse_node(root->pointer[index],data);
+      if(temp != NULL){
+        root->pointer[index] = temp->pointer[1];
+        insert_into_node(root,temp->array[0],index,temp->pointer[0]);
+      }
+      else return NULL;
+  }
+    else{
+      insert_into_node(root,data,index,NULL);
+      if(root->length == 5){
+        temp = split_node(root);
+        return temp;
+      }
+    }
+    if(root->length == 5) return split_node(root);
+    //for(int i = length; i > index;i--){
+    //  root->array[i] = root->array[i-1];
+    //}
+   // root->array[index] = data;
+    //root->length++;
   }
 }
 int main(){
@@ -61,8 +111,10 @@ int main(){
         continue;
       case 1:
         if(root->length < 5){
-          insert_in_node(root);
-          printf("%d\n",root->length);
+          int data;
+          printf("Enter the data: ");
+          scanf("%d",&data);
+          root = traverse_node(root,data);
         }
         else
           printf("Root is full\n");
