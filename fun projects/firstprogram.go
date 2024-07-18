c446aeb5009a0efd111d68beeedddadd063d091a
vcgithub41210 <vinayc.11b.kmbvm@gmail.com>
2024-07-18 22:19:00 +0530

    go journey lets goo

---
+++ fun projects/firstprogram.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main(){
+  //single line comment
+  /*
+  multi line comment - both comments similar to c language
+  */
+  // for some reason it did not compile the source code quickly the first time i run it
+  var idk_to_name string = "vinay"
+  fmt.Println("Hello " + idk_to_name)
+}
