ffece4c65c0b1ab1be65a68463bb09ade2476e0a
vcgithub41210 <vinayc.11b.kmbvm@gmail.com>
2024-08-19 21:06:53 +0530

    fcfs

--- C_programs/systemsoftwarelab/all_scheduling_algos.c
+++ C_programs/systemsoftwarelab/all_scheduling_algos.c
@@ -7,8 +7,42 @@ void display(int num_p,int **array){
     printf("%d\t\t%d\t\t\t%d\t\t\t%d\t\t\t%d\t\t%d\n",i+1,*(*(array)+i),*(*(array + 1)+i),*(*(array + 2)+i),*(*(array + 3)+i),*(*(array + 4)+i));
 	}
 }
+void sort_at(int num_p,int **array){
+  for (int i = 0; i < num_p-1;i++){
+    int min = i;
+    for(int j = i+1;j < num_p;j++){
+      if(*(*(array+1)+j) < *(*(array+1)+min)){
+    min = j; 
+      }
+    }
+    if (min != i){
+    int temp = *(*(array+1)+min);
+    *(*(array+1)+min) = *(*(array+1)+i);
+    *(*(array+1)+i) = temp;
+    int temp1 = *(*(array)+min);
+    *(*(array)+min) = *(*(array)+i);
+    *(*(array)+i) = temp1;
+    }
+  }
+  for(int i = 0; i < num_p;i++){
+    *(*(array+3)+i) = -(*(*(array+1)+i));
+  }
+}
 //functions
-void FCFS(){}
+void FCFS(int num_p,int **array){
+  // array+2 ==> completion time
+  // array + 3 ==> waiting time
+  // array + 4 ==> turn arount time
+  *(*(array+2)) = *(*(array));
+  for(int i = 1; i < num_p; i++){
+    int prev_ct = *(*(array+2)+i-1);
+    *(*(array+2)+i) = prev_ct + *(*(array)+i);
+    *(*(array+3)+i) += prev_ct;
+  }
+  for (int i = 0; i < num_p;i++){
+    *(*(array+4)+i) = *(*(array)+i) + *(*(array+3)+i);
+  }
+}
 void SJF(){}
 void SRTF(){}
 void Round_Robin(){}
@@ -38,9 +72,12 @@ int main(){
 		printf("Enter the arrival time of the processes: ");
 		scanf("%d",*(array+1)+i);
 	}
-  display(num_p,array);
+  sort_at(num_p,array);
   int choice;
   do{
+    display(num_p,array);
+    if(choice == -1)
+        printf("Invalid option: ");
     printf("\n1)FCFS\t2)SJF\t3)SRTF\t4)Round_Robin\t5)Priority\t0)QUIT\n");
     printf("Enter the algorithm to perform: ");
     scanf("%d",&choice);
@@ -48,7 +85,7 @@ int main(){
       case 0:
         continue;
       case 1:
-        FCFS();
+        FCFS(num_p,array);
         break;
       case 2:
         SJF();
@@ -63,7 +100,7 @@ int main(){
         Priority();
         break;
       default:
-        printf("Invalid option: ");
+        choice = -1;
     }
   }
   while(choice != 0);
